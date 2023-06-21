eb487446eba031fd8cd9120d46a3e563dc78a374
chayoung-choi <chayoung.dev@gmail.com>
2023-06-21 18:15:19 +0900

    Initial commit

---
+++ src/main/java/com/eden/sbcorealpha/SbCoreAlphaApplication.java
@@ -0,0 +1,13 @@
+package com.eden.sbcorealpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCoreAlphaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbCoreAlphaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/eden/sbcorealpha/SbCoreAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eden.sbcorealpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCoreAlphaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
